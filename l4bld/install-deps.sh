6c46472be35d50821a5ce221a6b10cb087cf0374
Jeff Garzik <jgarzik@bitpay.com>
2015-01-23 15:04:43 -0500

    Import from Google Doc.

---
+++ l4bld/install-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+apt-get install build-essential libtool autotools-dev\ autoconf pkg-config libssl-dev
+apt-get install libboost-all-dev
+apt-get install libdb5.1++-dev
+apt-get install libminiupnpc-dev
+apt-get install libqt5gui5 libqt5core5 libqt5dbus5\ qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+apt-get install libqrencode-dev
+apt-get install git-all
---
+++ l4tx/mkspend.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./bitcoin-tx -testnet -create in=f6d24d5910adc780d0b320ae2ebc769a58535fb33aa027999e19d61268a2e632:1 outaddr=0.78:mhrwhPPPtCiqeXqzmTz9ozih96CCkC8ENS > tx.out
